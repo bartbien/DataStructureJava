676018902844e3784b226045f64b8deeafc4044e
BartBien <phoenixjcam@gmail.com>
2014-09-28 10:59:32 +0200

    List interface implementation. ArrayList, LinekedList fundamental methods and fields

---
+++ src/com/phoenixjcam/collection/list/ArrayList.java
@@ -0,0 +1,62 @@
+package com.phoenixjcam.collection.list;
+
+/**
+ * 
+ * @author Bart88
+ *
+ * @param <E>
+ */
+public class ArrayList<E> implements List<E>
+{
+	Object[] elementData;
+
+	@Override
+	public void add(E element)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(int index, E element)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void get(int index)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void set(int index, E element)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(int index)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isEmpty()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/com/phoenixjcam/collection/list/LinkedList.java
@@ -0,0 +1,92 @@
+package com.phoenixjcam.collection.list;
+
+/**
+ * 
+ * 
+ * @author Bart88
+ *
+ * @param <E>
+ */
+public class LinkedList<E> implements List<E>
+{
+	private int size;
+
+	private Node<E> first;
+	private Node<E> last;
+
+	public LinkedList()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	private static class Node<E>
+	{
+		E item;
+		Node<E> next;
+		Node<E> last;
+
+		public Node(Node<E> prev, E element, Node<E> next)
+		{
+		}
+	}
+
+	public E getFirst()
+	{
+		return null;
+	}
+
+	public E getLast()
+	{
+		return null;
+	}
+
+	@Override
+	public void add(E element)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(int index, E element)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void get(int index)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void set(int index, E element)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(int index)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isEmpty()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
