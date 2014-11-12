6a647884949ddf452f2c108661d1be330248302a
BartBien <phoenixjcam@gmail.com>
2014-11-12 11:23:30 +0100

    Common utils numbers - Standard dictionary numbers

---
+++ performance/utils/CommonUtils.java
@@ -0,0 +1,21 @@
+package utils;
+
+/**
+ * <pre> 
+ * http://en.wikipedia.org/wiki/Names_of_large_numbers 
+ * Short scale (U.S., Canada and modern British)
+ * 
+ * @author BartBien
+ *
+ */
+public class CommonUtils
+{
+	public static final int hundred =         100;
+	public static final int thousand =        1000;
+	public static final int tenThousand =     10000;
+	public static final int hundredThousand = 100000;
+	public static final int million =         1000000;
+	public static final int tenMillion =      10000000;
+	public static final int hundredMillion =  100000000;
+	public static final int billion =         1000000000;
+}
