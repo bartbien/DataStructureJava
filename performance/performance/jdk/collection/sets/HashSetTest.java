ceedb27ae475207fceeb91076e4158e1a9bac615
BartBien <phoenixjcam@gmail.com>
2014-12-07 22:24:43 +0100

    Sets update

--- performance/performance/jdk/collection/sets/HashSetTest.java
+++ performance/performance/jdk/collection/sets/HashSetTest.java
@@ -2,5 +2,5 @@ package performance.jdk.collection.sets;
 
 public class HashSetTest
 {
-
+	java.util.HashSet<String> hashSet;
 }
--- performance/performance/jdk/collection/sets/TreeSetTest.java
+++ performance/performance/jdk/collection/sets/TreeSetTest.java
@@ -2,5 +2,5 @@ package performance.jdk.collection.sets;
 
 public class TreeSetTest
 {
-
+	java.util.TreeSet<String> treeSet;
 }
